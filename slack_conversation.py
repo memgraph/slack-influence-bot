fde792c642a29b579ce479b2e5fe623f1687af96
Toni Lastre <toni.lastre@memgraph.io>
2021-08-27 14:45:10 +0200

    Add messages for personal, channel and relationship

--- slack_conversation.py
+++ slack_conversation.py
@@ -30,14 +30,19 @@ def get_lazy_response() -> str:
         f"Hey, {get_incorrect_today_day()} so give me a break. I will get you the response in a sec!",
         "Woah, thanks for reaching out. Bare with me, I am preparing an answer :wink:",
         "While you wait, you can learn a bit about <https://www.youtube.com/watch?v=dQw4w9WgXcQ|graphs>!",
-        "Today I am really slow, it was a drinking night. I am packaging the response",
-        "Just a second...",
+        "Today I am really slow, yesterday was a drinking night but nobody told me to stop so I am totally wasted...",
+        "Goddammit not today!",
         "It takes a bit to get results so I would like to share a crazy story I heard: So apparently there " + \
-        "was a team that managed to infiltrate a double agent into a company saying it will do recommendations, " + \
-        "analytics, and all other buzzwords (Machine Learning :joy:) but they used it to spread gossip. What a time " + \
+        "was a team that managed to infiltrate a Slack bot into a company saying it will do recommendations, " + \
+        "analytics, and all other buzzwords (Machine Learning :joy:) but they used it just to spread gossip. What a time " + \
         "to be alive, ha?",
         "While we wait for the results, let me tell you a joke: Helvetica and Times New Roman walk into a bar... " + \
         "Meh, forget it, it is bad anyway.",
+        "My philosophy is, basically this. And this is something that I live by. And I always have. And I always will. Don’t, ever, for any reason, do anything, to anyone, for any reason, ever, no matter what, no matter where, or who you are with, or, or where you are going, or, or where you’ve been. Ever. For any reason. Whatsoever.",
+        "Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked. But it’s not like this compulsive need like my need to be praised.",
+        "Wikipedia is the best thing ever. Anyone in the world can write anything they want about any subject so you know you are getting the best possible information.",
+        "Guess what? I have flaws. What are they? Oh I dunno, I sing in the shower? Sometimes I spend too much time volunteering. Occasionally I’ll hit somebody with my car. So sue me.",
+        "While we wait for the results, look, it doesn’t take a genius to know that every organization thrives when it has two leaders. Go ahead, name a country that doesn’t have two presidents; a boat that sets sail without two captains. Where would Catholicism be without the popes?",
     ])
 
 
@@ -62,3 +67,14 @@ def get_unknown_channel_response(formatted_channel_name) -> str:
         f"Sorry sorry, but I am not part of the channel {format_channel_id_for_slack_message(channel_id)}. Aaaand it " + \
         "should be a public channel too.",
     ])
+
+
+def get_personal_prefix_response() -> str:
+    return random.choice([
+        "Hello fellow Slacker! You've come to me to get some answers, yes?",
+        "I present you...",
+        "I must once again ask you to venture into your history, Slacker.",
+        "No, no data for you! I said NO! ... Fine, have it.",
+        "Do you feel it as I do, Slacker? A curiosity as pure and potent as sunshine?",
+    ])
+
--- slack_stream.py
+++ slack_stream.py
@@ -34,10 +34,11 @@ from slack_bolt import App
 from gqlalchemy import Memgraph
 from kafka import KafkaProducer
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from slack_history import get_channel_by_id, get_user_by_id
+from slack_history import get_channel_by_id, get_user_by_id, get_channels
 from slack_conversation import get_lazy_response, get_missing_user_response, \
     get_invalid_self_check_user_response, get_unknown_channel_response, \
-    format_user_id_for_slack_message, format_channel_id_for_slack_message
+    format_user_id_for_slack_message, format_channel_id_for_slack_message, \
+    get_personal_prefix_response, get_incorrect_today_day
 
 logging.basicConfig(format="%(asctime)-15s [%(levelname)s]: %(message)s")
 logger = logging.getLogger("slack_bot")
@@ -104,36 +105,140 @@ def _get_message_influence_text(memgraph, event):
         RETURN DISTINCT w.value as word, sum(rating) as score, count(m) as messages, avg_reactions
         ORDER BY score DESC
     """)
-    words = [f"`{result['word']}`" for word in list(results)][:5]
+    words = [f"`{result['word']}`" for result in list(results)][:5]
+    channels = get_channels()
+    channel_text = ', '.join([format_channel_id_for_slack_message(channel['id']) for channel in channels])
+    text = "I can't really help you - I don't know you well. But no worries, I have a plan: " + \
+           "Buy me a beer and let's hang out so we can meet each other. :wink:\n\n" + \
+           "Btw also check if you are in the correct channel. I can help you only by checking the messages from " + \
+           f"the channels that I can see: {channel_text}"
+    if len(words) > 0:
+        text = f"I know exactly what to do. But in a much more real sense, I have no idea what to do.\n\n" + \
+               f"But, {get_incorrect_today_day()} and I am in a good mood to give recommendations, especially to you " + \
+               f"{format_user_id_for_slack_message(event['user_id'])}, my best friend.\n\n" + \
+               f"See if you can somehow mention the following words in your message: {', '.join(words)}"
 
-    return f"You want me to influence this? {', '.join(words)}\n\n_Your original message:_\n{message}"
+    return f"{text}\n\n_Your original message:_\n{message}"
+
+
+def _get_channel_highest_values_results(memgraph, channel_id):
+    query_prefix = ""
+    if channel_id:
+        query_prefix = f"""
+        MATCH (c:Channel {{ uuid: "{channel_id}" }})<--(m:Message)
+        WITH m
+        """
+
+    query = f"""
+        {query_prefix}
+        MATCH (m:Message)<-[r1:REACTED_ON]-(u:User)
+        OPTIONAL MATCH (m)<-[r2:REACTED_ON]-(u:User)
+        WHERE r2.reaction =~ "value.*"
+        RETURN DISTINCT u.uuid as user, count(r1) as total_reactions, count(r2) as value_reactions
+        ORDER BY value_reactions DESC, total_reactions ASC
+        LIMIT 5
+    """
+    return list(memgraph.execute_and_fetch(query))
+
+
+def _get_channel_lowest_values_results(memgraph, channel_id):
+    query_prefix = ""
+    if channel_id:
+        query_prefix = f"""
+        MATCH (c:Channel {{ uuid: "{channel_id}" }})<--(m:Message)
+        WITH m
+        """
+
+    query = f"""
+        {query_prefix}
+        MATCH (m:Message)<-[r1:REACTED_ON]-(u:User)
+        OPTIONAL MATCH (m)<-[r2:REACTED_ON]-(u:User)
+        WHERE r2.reaction =~ "value.*"
+        RETURN DISTINCT u.uuid as user, count(r1) as total_reactions, count(r2) as value_reactions
+        ORDER BY value_reactions ASC, total_reactions DESC
+        LIMIT 5
+    """
+    return list(memgraph.execute_and_fetch(query))
 
 
 def _get_channel_influence_text(memgraph, event):
     channel_id = event["channel_id"]
-    results = list(memgraph.execute_and_fetch(f"""
-        MATCH (c:Channel {{ uuid: '{channel_id}' }})
-        RETURN c as channel
-    """))
-    db_channel = results[0].get("channel") if len(results) > 0 else None
-    if not db_channel:
-        return get_unknown_channel_response(channel_id)
+    is_all_channels = re.match("(tell|give)?\s*(me)?\s*(all|everything)", event["text"].lower())
 
-    # TODO: Call the communities here
-    # TODO: Find a channel in the database
-    return f"You want to influence the channel {format_channel_id_for_slack_message(channel_id)}"
+    if not is_all_channels:
+        results = list(memgraph.execute_and_fetch(f"""
+            MATCH (c:Channel {{ uuid: '{channel_id}' }})
+            RETURN c as channel
+        """))
+        db_channel = results[0].get("channel") if len(results) > 0 else None
+        if not db_channel:
+            return get_unknown_channel_response(channel_id)
+
+    results_highest = _get_channel_highest_values_results(memgraph, None if is_all_channels else channel_id)
+    results_lowest = _get_channel_lowest_values_results(memgraph, None if is_all_channels else channel_id)
+
+    text = f"So, I just went to see what is going on in {format_channel_id_for_slack_message(channel_id)} regarding our values."
+    if is_all_channels:
+        text = "Ooh, we have someone who knows cheats. Ok, ok, ok. So you want to know everything, all the channels? :partyparrot:\n"
+        text += "I will check value reactions in the following channels:\n"
+        channels = get_channels()
+        for channel in channels:
+            text += f"-   {format_channel_id_for_slack_message(channel['id'])}\n"
+
+    text += "\n\n"
+    text += ":value-relationship: :value-lead: :value-learn: :value-communicate: :value-invent:"
+
+    text += "\n\n"
+    text += "Who is giving the most `value-` reactions?\n"
+    if len(results_highest) == 0:
+        text += "   - Nobody :cry:"
+    else:
+        for result in results_highest:
+            text += f"   - *{result['value_reactions']}* - {format_user_id_for_slack_message(result['user'])}\n"
+
+    text += "\n\n"
+    text += "Who is not really reacting with `value-` reactions?\n"
+    if len(results_lowest) == 0:
+        text += "   - Nobody :cry:"
+    else:
+        for result in results_lowest:
+            text += f"   - *{result['value_reactions']}* - {format_user_id_for_slack_message(result['user'])}\n"
+
+    return text
 
 
 def _get_personal_influence_text(memgraph, event):
     self_user_id = event["user_id"]
-    # TODO: Call the aggregation of a User here
     results = list(memgraph.execute_and_fetch(f"""
-        MATCH (u:User {{ uuid: '{self_user_id}' }})-->(m:Message)
-        RETURN count(m) as message_count
+        MATCH (u:User {{ uuid: "{self_user_id}" }})-[p:POSTED]-(m:Message)-[c:CONTAINS]-(w:Word)
+        WITH DISTINCT w.value as word_value, count(*) AS word_count
+        RETURN word_value, word_count
+        ORDER BY word_count DESC
+        LIMIT 3
     """))
-    count = results[0].get("message_count", 0) if len(results) > 0 else 0
 
-    return f"You are not so active, you just posted {count} messages"
+    results_reactions = list(memgraph.execute_and_fetch(f"""
+        MATCH (u:User {{ uuid: "{self_user_id}" }})-[r:REACTED_ON]->(m:Message)
+        WITH DISTINCT r.reaction AS reaction , count(*) AS reaction_count
+        RETURN reaction, reaction_count
+        ORDER BY reaction_count DESC
+        LIMIT 3
+    """))
+
+    text = get_personal_prefix_response()
+    text += "\n\n"
+
+    if len(results) == 0:
+        text += "Hmmmm, you are not really active on Slack. I can't help you. Be more active!"
+    else:
+        words = ", ".join([f"`{r['word_value']}`" for r in results])
+        text += f"Oh my, you must have talk about {words} a lot!"
+
+    if len(results_reactions) > 0:
+        reactions = " ".join([f":{r['reaction']}:" for r in results_reactions])
+        text += f" I see you like to react with {reactions}. Nice choice!"
+
+    return text
 
 
 def _get_relationship_influence_text(memgraph, event):
@@ -146,8 +251,19 @@ def _get_relationship_influence_text(memgraph, event):
     if self_user_id == another_user_id:
         return get_invalid_self_check_user_response()
 
-    # TODO: Call the Cypher between two Users here
-    return f"Ok ok, I will check the influence between you and {format_user_id_for_slack_message(another_user_id)}"
+    results = list(memgraph.execute_and_fetch(f"""
+        MATCH (:User {{ uuid: '{self_user_id}' }})-[:POSTED]->(m:Message)<-[r:REACTED_ON]-(:User {{ uuid: '{another_user_id}' }})
+        WITH r.reaction as reaction, COUNT(*) AS reaction_count
+        RETURN reaction, reaction_count
+        ORDER BY reaction_count DESC
+        LIMIT 5
+    """))
+    reactions = " ".join([f":{r['reaction']}:" for r in results])
+
+    if not reactions:
+        return f"I think you and {format_user_id_for_slack_message(another_user_id)} should have a beer and talk. I don't see too much interaction. :cry:"
+
+    return f"Let's keep this a secret, but {format_user_id_for_slack_message(another_user_id)} likes to add the following reactions to your messages: {reactions}"
 
 
 def _get_slack_app(bot_token: str, memgraph, event_handler=None):
@@ -206,12 +322,13 @@ def _get_slack_app(bot_token: str, memgraph, event_handler=None):
         _handle_command(body)
 
     def _handle_event(event):
-        logger.info(f"New event: {json.dumps(event)}")
+        logger.info(f"New processed event: {json.dumps(event)}")
         if event_handler:
             event_handler(event)
 
     @app.event("message")
     def handle_message_event(event):
+        logger.info(f"New event: {json.dumps(event)}")
         processed_event = dict(
             **event,
             channel_data=get_channel_by_id(event["channel"]),
@@ -220,6 +337,7 @@ def _get_slack_app(bot_token: str, memgraph, event_handler=None):
 
     @app.event("reaction_added")
     def handle_reaction_added_event(event):
+        logger.info(f"New event: {json.dumps(event)}")
         processed_event = dict(
             **event,
             user_data=get_user_by_id(event["user"]))
@@ -227,6 +345,7 @@ def _get_slack_app(bot_token: str, memgraph, event_handler=None):
 
     @app.event("reaction_removed")
     def handle_reaction_removed_event(event):
+        logger.info(f"New event: {json.dumps(event)}")
         processed_event = dict(
             **event,
             user_data=get_user_by_id(event["user"]))
